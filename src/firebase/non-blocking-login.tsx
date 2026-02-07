53f6386e363cfddd87b20d8f12997b5618860043
Rohith D <drohith7080@gmail.com>
2026-02-07 18:01:31 +0000

    when i logout it will logout if i again try to login it is taking only o

--- src/firebase/non-blocking-login.tsx
+++ src/firebase/non-blocking-login.tsx
@@ -32,6 +32,9 @@ export function initiateEmailSignIn(authInstance: Auth, email: string, password:
 /** Initiate Google sign-in (non-blocking). */
 export function initiateGoogleSignIn(authInstance: Auth): void {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
     // CRITICAL: Call signInWithPopup directly. Do NOT use 'await signInWithPopup(...)'.
     signInWithPopup(authInstance, provider);
 }

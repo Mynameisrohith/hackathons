f04ef8365a5fbc1b5788b30bb2e477b69cb35f24
Rohith D <drohith7080@gmail.com>
2026-02-10 16:56:00 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://wallpap

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'wallpaperaccess.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
